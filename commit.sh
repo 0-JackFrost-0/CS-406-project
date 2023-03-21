192f6ab3b5560cb67f20aa07f2f0da2aba63bc1e
Om Godage <omgodage@Oms-MacBook-Pro.local>
2023-03-21 16:16:07 +0530

    updated commit

--- commit.sh
+++ commit.sh
@@ -9,8 +9,16 @@ else
         if [[ -d $1 ]]
         then
             backup_dir=$(date +'%m_%d_%Y')
-            mkdir .repo/snapshots/$backup_dir
-            rsync -r --exclude='.repo' ./$1 .repo/snapshots/$backup_dir
+            # mkdir .repo/snapshots/$backup_dir
+            # rsync -r --exclude='.repo' ./$1 .repo/snapshots/$backup_dir
+            cd $1
+            for FILE in *; do
+                checksum="$(sha1sum $FILE)"
+                IFS=' ' read -ra hash <<< "$checksum"
+                mkdir ../.repo/snapshots/${hash:0:2}
+                touch ../.repo/snapshots/${hash:0:2}/${hash:2}
+                cp $FILE ../.repo/snapshots/${hash:0:2}/${hash:2}
+            done
         else 
             echo "Repository Does Not exist"
         fi
